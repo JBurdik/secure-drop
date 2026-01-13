f5495ad21d1fc04d2c5b8bfbca961a0b2283b472
JirkaB <burdik6@gmail.com>
2026-01-13 22:44:06 +0100

    build fix

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,10 +1,9 @@
 import { getAuthConfigProvider } from "@convex-dev/better-auth/auth-config";
 import type { AuthConfig } from "convex/server";
 
-const authDomain =
-  process.env.SITE_URL || "https://site.secure-drop.burdych.net";
+// const authDomain =
+//   process.env.SITE_URL || "https://site.secure-drop.burdych.net";
 
 export default {
-  providers: [getAuthConfigProvider()]
-  ],
+  providers: [getAuthConfigProvider()],
 } satisfies AuthConfig;
